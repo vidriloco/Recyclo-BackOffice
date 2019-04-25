46d2b83cc47e52c26e80edb62eb80ca7aac3ec00
Alejandro Cruz <vidriloco@gmail.com>
2019-04-25 12:33:44 -0500

    Adds user model and it's field validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   include Clearance::User
+  
+  validates :username, length: { within: 4..12 }
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { within: 6..20 }
 end
