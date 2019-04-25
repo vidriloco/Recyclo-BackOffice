e5cddef902120621a69028a082e3a1e1af3ab79e
Alejandro Cruz <vidriloco@gmail.com>
2019-04-25 12:34:16 -0500

    Adds users controller and routes

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::UsersController < Api::BaseController
+  def create
+    @user = user_from_params
+    
+    if @user.save
+      sign_in @user
+      render json: { token: @user.remember_token }, status: 200
+    else
+      render json: { error: @user.errors.full_messages.first }, status: 400
+    end
+  end
+  
+  def user_from_params
+    name = user_params.delete(:name)
+    username = user_params.delete(:username)
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+
+    Clearance.configuration.user_model.new.tap do |user|
+      user.name = name
+      user.username = username
+      user.email = email
+      user.password = password
+    end
+  end
+
+  def user_params
+    params[Clearance.configuration.user_parameter] || Hash.new
+  end
+end
