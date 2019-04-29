c6388911c28a8d9e38ca0b75dd4ca9250f5fcc2a
Alejandro Cruz <vidriloco@gmail.com>
2019-04-29 12:05:43 -0500

    Refactoring and adding of token based authentication

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,30 @@
 class Api::BaseController < ApplicationController
   protect_from_forgery prepend: true
+  
+  protected
+
+  def is_authorized
+    if user_with_token.nil?
+      render json: { error: "Parece ser que no estás logeado :/" }, status: 400
+    end
+  end
+
+  def user_with_token
+    return unless api_token
+    User.where(remember_token: api_token).first
+  end
+
+  def authenticate_via_token
+    user = user_with_token
+    sign_in user if user
+    cookies.delete(:remember_token) # so non-browser clients don't act like browsers and persist sessions in cookies
+  end
+
+  private
+
+  def api_token
+    pattern = /^Bearer /
+    header  = request.env["HTTP_AUTHORIZATION"]
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
-  
-  protected
-
-  def authenticate_via_token
-    return unless api_token
-    user = User.first.where(remember_token: api_token)
-    sign_in user if user
-    cookies.delete(:remember_token) # so non-browser clients don't act like browsers and persist sessions in cookies
-  end
-
-  private
-
-  def api_token
-    pattern = /^Bearer /
-    header  = request.env["HTTP_AUTHORIZATION"]
-    header.gsub(pattern, '') if header && header.match(pattern)
-  end
 end
